9bcb97b17eecf9bac88e3cd820026c0c605017bb
ZHAO Wenlong <wenlzhao@gmail.com>
2012-10-20 03:02:07 +0800

    correct some errors when the code is running in corona env

--- localService.lua
+++ localService.lua
@@ -4,53 +4,43 @@
 ---The function is to display all users's location based on user's location on screen ----
 ---It is built on Corona middle layer
 
-function networkListener ()
 
-	if (event.isError) then
-
-	    print("Network error - download failed" .. event.response)
-
-      return
-	else
+function constructData ()
 
 	     --download
-
         --Check whether the file exists, if yes, remove it
-        local path = system.pathForFile(  "pos.txt", system.DocumentsDirectory )
+       local path = system.pathForFile(  "pos.txt", system.DocumentsDirectory )
         local fhd = io.open( path )
 
 	    if fhd then
 
-             print( "File exists" )
+             print( "File pos.txt exists" )
+			 print( "Remove it")
 
-	         fhd.close()
+	         --fhd.close()
 
 	         local results, reason = os.remove( path )
 
          end
-
-		network.download("http://www.cse.cuhk.edu.hk/~tklam/pos.txt","GET",networkListener,"pos.txt",system.DocumentsDirectory)
-
 
 	         -- read the data file downloaded and parse it
 
 		local  MaxLongitude,MaxLatitude = 0,0
 		local userLongitude,userlatitude,relativeLongitude,relativeLatitude = 0, 0, 0, 0
 
-		local path = system.pathForFile("pos.txt", system.DocumentsDirectory)
           --  local filename = "d:/Dropbox/workspace/mobileApp/locService/pos.txt"
 
 		-- i is used to remember the user id
 		local i = 0
 
-		for  line in io.lines(filename)  do
+		for  line in io.lines(path)  do
 
 			i = i + 1
 			--print("line:" ..line)
-		    local _,_,id,name,longitude,latitude,altitude= string.find(line, '(%d+)%s+(%w+)%s+([-+]*[0-9]\.?[0-9]*)%s+([-+]*[0-9]\.?[0-9]*)%s+([^%s]+)')
+		    local _,_,id,name,longitude,latitude,altitude= string.find(line, "(%d+)%s+(%w+)%s+([-+]?[0-9]*\.?[0-9]*)%s+([-+]?[0-9]*\.?[0-9]*)%s+([^%s]+)")
 			 --local id,name,longitude,latitude = 1,"s0123456789",13, 0.598
 
-             --print("id:" .. id .. ",name:" .. name .. ",longitude:" .. longitude .. ",latitude:" .. latitude)
+             print("id:" .. id .. ",name:" .. name .. ",longitude:" .. longitude .. ",latitude:" .. latitude)
 
              --construct the Bounding box based on maximum and Minimum
              ----which means that if the furthest user can be shown, all users can be shown
@@ -86,16 +76,14 @@ function networkListener ()
 			 id=tonumber(id)
 		    userinfo[i] = { id, name,latitude, longitude,relativeLatitude,relativeLongitude}
 
-		    print("getid:" .. id .. ",name:" .. name .. ",longitude:" .. longitude .. ",latitude:" .. latitude)
-            --print("relativeLatitude:" .. relativeLatitude .. ",relativeLongitude:" .. relativeLongitude)
+		    --print("getid:" .. id .. ",name:" .. name .. ",longitude:" .. longitude .. ",latitude:" .. latitude)
+            --print("relativeLatitude:" .. relativeLatitude .. ",relativeLongitude:" .. relativeLongitude .. ",userinfo:" .. #userinfo)
 		 end
 
-	 --print("MaxLongitude:" .. MaxLongitude .. ",MaxLatitude:" .. MaxLatitude)
+	  --print("MaxLongitude:" .. MaxLongitude .. ",MaxLatitude:" .. MaxLatitude)
 
 		 -- store the max info in table configinfo
 	 configinfo ={MaxLatitude,MaxLongitude}
-
-    --end
 
 end
 
@@ -109,22 +97,22 @@ function tapListener1(event)
 end
 
 
-function tapListener2(event)
-         print("remove the text on screen")
+function tapListener(event)
+         print("remove the name on screen")
 
 		if circle.name ~= nil then
           circle.name.isVisible = false
          end
 
 		 --The text will be displayed for 5 seconds, and removed
-		 timer:performWithDelay( 5000, tapListener2 )
+		 timer:performWithDelay( 5000, tapListener )
 
 
 		 return true
 end
 
-function addListener1()
-		circle:addEventListener("tap", tapListener1)
+function addListener()
+		circle:addEventListener("tap", tapListener)
 
 end
 
@@ -143,7 +131,8 @@ function displayResult()
 
 	--define the 1st as the user, who should be in the ceneter and be in red color
 	-- which means that index of userinfo is 1,so
-	local id = userinfo[1][1]
+
+	--local id = userinfo[1][1]
 	--print(" 1st user's id:" ..id)
 
 	local defaultxpos = display.contentWidth / 2
@@ -158,6 +147,8 @@ function displayResult()
 	 -- user relative physical coordinates on screen
 	local userxpos, userypos
 
+	-- there is a overlapping issue here
+	--every time we have to remove the circle firstly when we begin to create it again
 
 	for index,value in ipairs(userinfo) do
         --print("index:" .. index)
@@ -188,6 +179,7 @@ function displayResult()
 		 print("id:" .. id .. ",userxpos:" .. userxpos .. ",userypos:" .. userypos)
 
 		 ---- create a new circle
+
 		 circle = display.newCircle(userxpos,userypos,15)
 
 		if index == 1 then
@@ -208,18 +200,45 @@ function displayResult()
 
 	end
 
-
      --if being tapped, the user name will be displayed
-	 addListener1()
+
+	 addListener()
 
 	-- display the text
-	myText=display.newText("Successfully get data",defaultxpos,defaultypos - 60,native.systemFont,16)
-	myText.x = display.contentWidth / 2
-	myText.y = display.contentHeight / 16
+	--print("defaultxpos:" ..defaultxpos .. ",defaultypos:" .. defaultypos)
+
+	local textxpos = defaultxpos - 250
+	local textypos= defaultypos + 270
+
+	local myText=display.newText("Successfully get data",defaultxpos,textypos,native.systemFont,20)
+	--myText.x = display.contentWidth / 2
+	--myText.y = display.contentHeight / 16
 	myText:setTextColor(255,255,255)
+
+
 end
 
 
+function listener(event)
+     network.download("http://www.cse.cuhk.edu.hk/~tklam/pos.txt","GET",networkListener,"pos.txt",system.DocumentsDirectory)
+end
+
+function networkListener(event)
+	if (event.isError) then
+
+	    print("Network error - download failed" .. event.response)
+
+      return
+	else
+
+	 -- construct the related user data
+     constructData();
+
+	 -- display the result on screen and interact with the user
+	 displayResult()
+
+	 end
+end
 ------ main ------
 
 -- variable
@@ -233,11 +252,8 @@ configinfo = {}
 ---- It is better to define a configuration file
 screenWidth,screenHeight =320,480
 
+--get data every 10 seconds
+timer.performWithDelay(10000,listener,0)
 
---get data every 10 seconds and construct the related user data
-timer.performWithDelay(10000,networkListener,0)
 --networkListener()
 --userinfo[1] = { 1, "s11222", 10, 10}
-
--- display the result on screen and interact with the user
-displayResult()
