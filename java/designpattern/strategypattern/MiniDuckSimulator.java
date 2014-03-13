69aae0361228e1d1fb4d9ee93c1db734f408ad39
muyun <wenlzhao@gmail.com>
2014-03-13 16:23:10 +0800

    git commit

--- java/designpattern/strategypattern/MiniDuckSimulator.java
+++ java/designpattern/strategypattern/MiniDuckSimulator.java
@@ -4,9 +4,6 @@ package strategypattern;
 
 public class MiniDuckSimulator {
     public static void main(String[] args){
-        //1. program to a supertype
-        //   exploit polymorphism by programming to a supertype so that the actual runtime object isn't locked into the code;
-        //   programming to an interface/supertype would be better, because it can assign the concrete implementation object at runtime
         Duck mallard = new MallardDuck();
         //call MallardDuck's inherited performQuack() method, which then delegates to the object's QuackBehavior
         mallard.performQuack();
