4c900aec57d3d71d5631810c0b941b25f1974bda
muyun <wenlzhao@gmail.com>
2015-01-11 02:15:01 +0800

    update

--- ruby/strcomb.rb
+++ ruby/strcomb.rb
@@ -2,68 +2,81 @@
 #   @author wenlong 
 #
 #   Functions:  
-#     1). remove the last columns for each file
-#
+#     1) remove the last columns for each file
+#     2)
+#     3) re-arrange the file format  
 
 #target dir
-#dir = "/home/zhaowenlong/workspace/proj/dev.mycode/ruby/source/2order/"
-dir = "/home/zhaowenlong/workspace/proj/dev.mycode/ruby/data/"
+dir = "/home/zhaowenlong/workspace/proj/dev.mycode/ruby/source/"
 #source = "/home/zhaowenlong/workspace/proj/dev.mycode/ruby/source/2order_/1_0.5.txt"
-source = dir + "2.txt"
+source = dir + "2order_0.05.txt"
  
 target = dir + "1_.txt"
 
+#GLOBAL ARRAY
 #store the data
 data = Array.new() 
+#store the index
+index = Array.new()
+
+#store the num of the flag 'COMPRESS_THRESHOLD'
+num = 0
 
 File.open(target, 'w') do | f |
    File.open(source).readlines.collect do | line |
       #for each line
-      puts "working on: #{line}"
-      
+      #puts "working on: #{line}"
+
       arr = line.chomp.split(";")
+
       #is the flag line
-      if arr.include?("COMPRESS_THRELOD")
+      if arr.include?("COMPRESS_THRESHOLD")
+          puts "working on: #{line}"
           arr_thre = Array.new()
           
-          #threshold = line.split(";")
-          puts  arr[1]
-          
-          #store each threshold
+          #store the threshold
           arr_thre.push(arr[1])
           
-          #put the array to global Array data
+          #put the array to GLOBAL Array data
           data.push(arr_thre)
-          
+
+          #store the flag num
+          num += 1
       else
-          arr_thre = data.pop()
-          #arr_thre.push(arr[0])
-          arr_thre.push(arr[1])
+          #get this Array arr_thre through GLOBAL Array data
+          arr_thre = data.pop()  #this is the num like '0.01'
           
+          #get the value
+          arr_thre.push(arr[1])
+          #store the value in data in ARRAY format
           data.push(arr_thre)
+
+          #store the index once to meet the output format         
+          if num == 1
+              index.push(arr[0])
+          end
           
       end 
-      
        
   end
   
-  #puts data.length()
-
+  #transpose data matrix  
   newdata = data.transpose
-  puts newdata
-  puts "---"
+  #meet the output format
+  index.insert(0,0)
   
+  puts  
+  puts "Give the output in #{target} ..."
+
   $i = 0
   while $i < newdata.length() do
       
-      #f.puts data.join(";") + "\n"
-     
       #store the threshold value
       #threshold.push(data[$i][0])
       #data[$i][0].pop()
       
-      puts newdata[$i].join(";")
-      f.puts newdata[$i].join(";")
+      #puts newdata[$i].insert(0,index.shift()).join(";")
+      f.puts newdata[$i].insert(0, index.shift()).join(";") + ";"
       $i += 1
   end
   
