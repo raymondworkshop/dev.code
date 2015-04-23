83a9005d4f50c7af040e7a264c2ed2d74bbf36c1
muyun <wenlzhao@gmail.com>
2015-04-23 22:33:39 +0800

    add

---
+++ c/getint.c
@@ -0,0 +1,36 @@
+//TODO: Need to the related functions and test
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* get next integer from input into *pn */
+
+int getint(int *pn )
+{
+  int c, sign;
+
+  while(isspace(c = getch())) /* skip white space */
+    ;
+
+  if( !isdigit(c) && c != EOF && c != '+' && c != '-')
+  {
+    ungetch(c);  /* not a number */
+    return 0;
+  }
+
+  sign = (c == '-') ? -1 : 1;
+  if (c == '+' || c == '-')
+    c = getch();
+  
+  for( *pn = 0; isdigit(c); c = getch())
+    *pn = 10 * *pn + (c - '0');
+  *pn *= sign;
+
+  if(c != EOF)
+    ungetch(c);
+  
+  return c;
+  
+}
+
