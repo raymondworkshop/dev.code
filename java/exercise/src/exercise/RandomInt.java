1c29c918b269297c2f58c801779ada41dd4d5ce9
muyun <wenlzhao@gmail.com>
2015-09-26 22:23:37 +0800

    review java

---
+++ java/exercise/src/exercise/RandomInt.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class RandomInt {
+	public static void main(String[] args){
+		//int N = Integer.parseInt(args[0]);
+		int N = StdIn.readInt();
+		
+		double r = Math.random();
+		
+		int n = (int)(r * N);
+		
+		System.out.println("The random integer is: " + n);
+		
+	}
+
+}
---
+++ java/exercise/src/exercise/SATmyYear.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class SATmyYear {
+	public static void main(String[] args){
+		double x = Double.parseDouble(args[0]);
+		StdOut.println((Gaussian.Phi(x) - 1019) / 209);
+	}
+
+}
