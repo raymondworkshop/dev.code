a66c7dbc2590d2552cb506397ba1dd346706a4c0
wenlzhao <wenlzhao@gmail.com>
2019-10-29 15:32:17 +0800

    update

---
+++ python/test_exercise_3.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+
+from exercise_3 import SimpleGradebook
+from exercise_3 import Gradebook
+
+class TestSimpleGradebook(unittest.TestCase):
+    #student = 'Isaac Newton'
+    book = SimpleGradebook()
+    book.add_student('Isaac Newton')
+    book.report_grade('Isaac Newton', 90)
+    print(book.average_grade('Isaac Newton'))
+
+
+class TestGradebook(unittest.TestCase):
+    book = Gradebook()
+    albert = book.student('Albert Einstein')
+    math = albert.subject('math')
+    math.report_grade(80, 0.10)
+    math.report_grade(92, 0.30)
+    #eng = albert.subject('eng')
+    #eng.report_grade(92, 0.15)
+    #
+    print(albert.average_grade())
+
+if __name__ == '__main__':
+    unittest.main()
